884ac4573834ca15f5533903a7a66287ee8bb922
Seymour_le_Grand <a@b.c>
2017-06-13 14:17:25 +0200

    Traceur avec notification

--- app/src/main/java/com/example/admin/androidtp9/Traceur.java
+++ app/src/main/java/com/example/admin/androidtp9/Traceur.java
@@ -1,5 +1,7 @@
 package com.example.admin.androidtp9;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -11,42 +13,52 @@ public abstract class Traceur extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         toast = Toast.makeText(this, "billy onCreate", Toast.LENGTH_LONG);
-        toast.show();
+        notify("OnCreate");
     }
     @Override
     protected void onPause(){
         super.onPause();
         toast = Toast.makeText(this, "billy onPause", Toast.LENGTH_LONG);
-        toast.show();
+        notify("onPause");
     }
     @Override
     protected void onResume(){
         super.onResume();
         toast = Toast.makeText(this, "billy onResume", Toast.LENGTH_LONG);
-        toast.show();
+        notify("onResume");
     }
     @Override
     protected void onStop(){
         super.onStop();
         toast = Toast.makeText(this, "billy onStop", Toast.LENGTH_LONG);
-        toast.show();
+        notify("onStop");
     }
     @Override
     protected void onDestroy(){
         super.onDestroy();
         toast = Toast.makeText(this, "billy onDestroy", Toast.LENGTH_LONG);
-        toast.show();
+        notify("onDestroy");
     }
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState){
         super.onRestoreInstanceState(savedInstanceState);
         toast = Toast.makeText(this, "billy onRestore", Toast.LENGTH_LONG);
-        toast.show();
+        notify("onRestoreInstanceState");
     }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         toast = Toast.makeText(this, "billy onSave", Toast.LENGTH_LONG);
-        toast.show();
+        notify("onSaveInstanceState");
+    }
+
+    private void notify(String methodName){
+        String name = this.getClass().getName();
+        String[] strings = name.split("\\.");
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle(methodName + " " + strings[strings.length-1])
+                .setAutoCancel(true).setSmallIcon(R.mipmap.ic_launcher).setContentText(name).build();
+        NotificationManager notificationManager =(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify((int) System.currentTimeMillis(), noti);
     }
 }
