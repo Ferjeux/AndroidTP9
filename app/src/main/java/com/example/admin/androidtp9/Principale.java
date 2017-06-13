54324491b9ca8b2beb38cdc293e0c562b9cacd84
Seymour_le_Grand <a@b.c>
2017-06-13 15:14:10 +0200

    ajout parcelable

--- app/src/main/java/com/example/admin/androidtp9/Principale.java
+++ app/src/main/java/com/example/admin/androidtp9/Principale.java
@@ -2,12 +2,44 @@ package com.example.admin.androidtp9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Principale extends Traceur {
+
+    private TextView textView;
+    Point point = new Point();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.notlose);
+        textView.setText(String.valueOf(point.getCompteur()));
+
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        point = savedInstanceState.getParcelable("value");
+        textView.setText(String.valueOf(point.getCompteur()));
+
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable("value", point);
+    }
+
+    public void plus(View view) {
+        point.setCompteur(point.getCompteur()+1);
+        textView.setText(String.valueOf(point.getCompteur()));
+    }
+
+    public void reset(View view) {
+        point.setCompteur(0);
+        textView.setText(String.valueOf(point.getCompteur()));
     }
 }
